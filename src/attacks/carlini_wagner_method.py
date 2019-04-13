Added functionality for the cochlear AI model and corrected snr calculation

src/attacks/carlini_wagner_method.py
@@ -26,7 +26,6 @@ class CarliniWagnerAttack():
 
     def build_attack(self,pcm):
         self.pcm = pcm
-        self.original_audio = tf.placeholder(tf.float32,shape=[None])
         self.modifier_init = tf.placeholder(tf.int32,name='mod_init')
         self.modifier = tf.Variable(tf.zeros(shape=self.modifier_init),validate_shape=False)
         self.labels = tf.placeholder(tf.int32,name='labels')
@@ -39,7 +38,7 @@ class CarliniWagnerAttack():
         self.output = self.model.get_logits()
         self.probs = self.model.get_probs()
 
-        self.other = (tf.tanh(self.original_audio)+1)/2.0*(self.clip_max-self.clip_min)+self.clip_min
+        self.other = (tf.tanh(self.pcm)+1)/2.0*(self.clip_max-self.clip_min)+self.clip_min
 
         self.l2dist = tf.reduce_mean(tf.square(self.newpcm-self.other))
        
@@ -56,8 +55,8 @@ class CarliniWagnerAttack():
         self.loss = self.loss1+self.loss2
         
         start_vars = set(x.name for x in tf.global_variables())
-        optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        self.train = optimizer.minimize(self.loss, var_list=[self.modifier])
+        self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+        self.train = self.optimizer.minimize(self.loss, var_list=[self.modifier])
         end_vars = tf.global_variables()
         new_vars = [x for x in end_vars if x.name not in start_vars]
 
@@ -66,7 +65,7 @@ class CarliniWagnerAttack():
     
     
     def attack(self,sess,input_audio,label,labels,batch_size):
-        
+        print('Audio len:',input_audio.shape) 
         lab = label
         def compare(x, y):
             if not isinstance(x, (float, int, np.int64)):
@@ -84,9 +83,10 @@ class CarliniWagnerAttack():
         batch_size=int(batch_size)
         
         original_audio = np.clip(input_audio,self.clip_min,self.clip_max)
-        input_audio = original_audio
         original_audio = np.arctanh(original_audio*.999999)
-         
+        
+        input_audio = original_audio
+
         b = np.zeros((batch_size, 41))
         b[np.arange(batch_size), labels] = 1
         labels = b
@@ -105,13 +105,14 @@ class CarliniWagnerAttack():
             
         for outer_step in range(self.binary_search_steps):
             sess.run([self.init],feed_dict={self.modifier_init:input_audio.shape})
+            sess.run(tf.variables_initializer(self.optimizer.variables()),feed_dict={self.modifier_init:input_audio.shape})
             if(outer_step == 0):
                 s = sess.run([self.probs],feed_dict={self.pcm:input_audio})
                 s = np.squeeze(s)
                 if(s.ndim==1):
-                    print(np.argmax(s),np.max(s))
+                    print('Original label:',np.argmax(s),'Original confidence:',np.max(s))
                 else:
-                    print(np.argmax(s,axis=1),np.max(s,axis=1))
+                    print('Original labels:',np.argmax(s,axis=1),'Original confidences:',np.max(s,axis=1))
                     s = np.mean(s,axis=0)
                 original_label = np.argmax(s)
                 original_confidence = np.max(s)
@@ -126,7 +127,7 @@ class CarliniWagnerAttack():
 
             prev = 1e6
             for iteration in range(self.max_iterations):
-                _,l,l2,score,npcm = sess.run([self.train,self.loss,self.l2dist,self.probs,self.newpcm],feed_dict={self.pcm:audio,self.original_audio:original_audio,self.labels:labels,self.const:CONST})
+                _,l,l2,score,npcm = sess.run([self.train,self.loss,self.l2dist,self.probs,self.newpcm],feed_dict={self.pcm:audio,self.labels:labels,self.const:CONST})
                     
                 l = np.squeeze(l)
                 l2 = np.squeeze(l2)
@@ -134,7 +135,7 @@ class CarliniWagnerAttack():
                 s = score
                 if(score.ndim !=1):
                     score = np.mean(score,axis=0)
-                if(iteration % ((self.max_iterations//10) or 1)==0):
+                if(iteration % ((self.max_iterations//5) or 1)==0):
                     print('Loss:',l)
                     print('L2 distance:',l2)
                     print('New label:',np.argmax(score))
@@ -158,11 +159,11 @@ class CarliniWagnerAttack():
                     o_bestattack = npcm
                     new_label = np.argmax(score)
                     new_confidence = np.max(score)
+                    new_confidence_original_label = score[original_label]
                     if(s.ndim==1):
-                        print(np.argmax(s),np.max(s),l2)
+                        print('New label:',np.argmax(s),'New confidence:',np.max(s),'L2 distortion:',l2)
                     else:
-                        print(np.argmax(s,axis=1),np.max(s,axis=1),l2,np.argmax(score),np.max(score))
-                        print(score)
+                        print('New labels:',np.argmax(s,axis=1),'New confidences:',np.max(s,axis=1),'L2 distortion:',l2,'Average label:',np.argmax(score),'Average confidence:',np.max(score))
                     
 
 
@@ -179,4 +180,4 @@ class CarliniWagnerAttack():
 
                 o_bestl2 = np.array(o_bestl2)
                 
-        return o_bestattack,original_label,original_confidence,new_label,new_confidence
+        return o_bestattack,original_label,original_confidence,new_label,new_confidence,new_confidence_original_label

src/attacks/deep_fool_method.py
@@ -39,18 +39,21 @@ class DeepFool():
         current = np.argmax(current)
 
         w = np.squeeze(np.zeros(sample.shape))
-        r_tot = np.zeros(sample.shape)
+        r_tot = np.squeeze(np.zeros(sample.shape))
         original = current
         while(np.any(current==original) and iteration <self.max_iter):
 
-            gradients = np.zeros([batch_size,self.nb_classes,adv_x.shape[0]])
+            gradients = np.zeros([batch_size,self.nb_classes,np.max(adv_x.shape)])
             for idx in range(batch_size):
                 predictions,g = sess.run([self.logits,self.grads],feed_dict={self.pcm:adv_x,self.batch_iter:idx})      
-                gradients[idx,:,:] = np.transpose(g)
+                g = np.squeeze(g)
+                if(g.shape[0] != self.nb_classes):
+                    g = np.transpose(g)
+                gradients[idx,:,:] = g
             
             pert = np.inf
 
-            w_k = np.zeros([batch_size,self.nb_classes,adv_x.shape[0]])
+            w_k = np.zeros([batch_size,self.nb_classes,np.max(adv_x.shape)])
             pert_k = np.zeros([batch_size,self.nb_classes])
 
             for idx in range(batch_size):

src/carlini_wagner_main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import librosa
 from tqdm import tqdm
-
+import time
 
 import config as cfg
 import file_io as io
@@ -12,7 +12,7 @@ import utils_tf
 import utils
 
 from attacks import carlini_wagner_method as CW
-from cleverhans_wrapper import CleverHansModel
+from cleverhans_wrapper import CleverHansModel,CochlearModel
 
 def carliniwagnertargeted(audio_path,metadata_path,model_path,exp_data_path,adv_audio_path,save_data=False):
     #Run the attacks to generate adversarial attacks on manually verified examples on the training and test data
@@ -56,7 +56,7 @@ def carliniwagnertargeted(audio_path,metadata_path,model_path,exp_data_path,adv_
             
             print('Ground truth label:',label_names[i]) 
             label = 15
-            adv,o_label,o_conf,n_label,n_conf = carliniwagner.attack(sess,data,label,np.repeat(label,int(q)),int(q))
+            adv,o_label,o_conf,n_label,n_conf,n_gt_conf = carliniwagner.attack(sess,data,label,np.repeat(label,int(q)),int(q))
                 
             if(save_data):
                 librosa.output.write_wav(adv_audio_path + 'adv-' + audio_file_name,adv,sample_rate)
@@ -70,7 +70,7 @@ def carliniwagnertargeted(audio_path,metadata_path,model_path,exp_data_path,adv_
             mean_sq_error.append(np.mean((adv-data)**2))
             std_dev.append(np.std((adv-data)**2))
             snr.append(10*np.log10(np.mean(data**2)/(np.mean(adv-data)**2)))
-            print(o_label,o_conf,n_label,n_conf,snr) 
+            print(o_label,o_conf,n_label,n_conf,snr,n_gt_conf) 
 
         if(save_data):
             df_cw = pd.DataFrame({'audio_name':audio_name,'audio_length':audio_length,'original_label':original_label,'original_confidence':original_confidence,'new_label':new_label,'new_confidence':new_confidence,'mean_square_error':mean_sq_error,'standard_deviation':std_dev,'SNR':snr})
@@ -89,7 +89,6 @@ def carliniwagneruntargeted(audio_path,metadata_path,model_path,exp_data_path,ad
     mel_fb = librosa.filters.mel(sr=32000,n_fft=1024,n_mels=64).T
     sample_rate = 32000
     
-    label_list = ["Acoustic_guitar", "Bass_drum", "Cello", "Chime", "Clarinet", "Cowbell", "Double_bass", "Electric_piano", "Flute", "Glockenspiel", "Gong", "Harmonica", "Hi-hat", "Oboe", "Saxophone", "Snare_drum", "Tambourine", "Trumpet", "Violin_or_fiddle"]
  
     audio_name = []
     audio_length = []
@@ -97,18 +96,17 @@ def carliniwagneruntargeted(audio_path,metadata_path,model_path,exp_data_path,ad
     original_confidence = []
     new_label = []
     new_confidence = []
-    mean_sq_error = []
-    std_dev = []
+    new_o_label_conf = []
     snr = []
     with tf.Graph().as_default() as graph:
         mel_filt = tf.convert_to_tensor(mel_fb,dtype=tf.float32)
         model = CleverHansModel(model_path +'.meta',sample_rate,generator,mel_filt)
         pcm = tf.placeholder(tf.float32,shape=[None],name='input_audio')
-        carliniwagner = CW.CarliniWagnerAttack(model,learning_rate = 5e-4,initial_const = 1e-2)
+        carliniwagner = CW.CarliniWagnerAttack(model,learning_rate = 5e-4,initial_const = 1e-2,max_iterations=500)
         saver = carliniwagner.build_attack(pcm)
     with tf.Session(graph=graph) as sess:
         saver.restore(sess,model_path)
-        for i in range(3,4):
+        for i in range(1):
             audio_file_name = file_names[i]
             try:
                 data,q = utils_tf._preprocess_data(audio_path,audio_file_name)
@@ -122,11 +120,12 @@ def carliniwagneruntargeted(audio_path,metadata_path,model_path,exp_data_path,ad
                  
          
             labels_batchwise = np.repeat(label,int(q))
-
-            adv,o_label,o_conf,n_label,n_conf = carliniwagner.attack(sess,data,label,labels_batchwise,int(q))
-            librosa.output.write_wav('car_adv.wav',adv,sample_rate)    
-            librosa.output.write_wav('car_real.wav',data,sample_rate)
             
+            tic = time.process_time()
+            adv,o_label,o_conf,n_label,n_conf,n_conf_gt = carliniwagner.attack(sess,data,label,labels_batchwise,int(q))
+            toc = time.process_time()
+
+            print('Time for iteration:',i,'is',toc-tic)
             if(save_data):
                 librosa.output.write_wav(adv_audio_path + 'adv-' + audio_file_name,adv,sample_rate)
             
@@ -136,14 +135,77 @@ def carliniwagneruntargeted(audio_path,metadata_path,model_path,exp_data_path,ad
             original_confidence.append(o_conf)
             new_label.append(n_label)
             new_confidence.append(n_conf)
-            mean_sq_error.append(np.mean((adv-data)**2))
-            std_dev.append(np.std((adv-data)**2))
+            new_o_label_conf.append(n_conf_gt)
             snr.append(10*np.log10(np.mean(data**2)/(np.mean((adv-data)**2))))
-            print(o_label,o_conf,n_label,n_conf,snr) 
+            print(snr)
         if(save_data):
-            df_cw = pd.DataFrame({'audio_name':audio_name,'audio_length':audio_length,'original_label':original_label,'original_confidence':original_confidence,'new_label':new_label,'new_confidence':new_confidence,'mean_square_error':mean_sq_error,'standard_deviation':std_dev,'SNR':snr})
+            df_cw = pd.DataFrame({'audio_name':audio_name,'audio_length':audio_length,'original_label':original_label,'original_confidence':original_confidence,'new_label':new_label,'new_confidence':new_confidence,'new_orig_conf':new_o_label_conf,'SNR':snr})
         
-            with open(exp_data_path,'a') as f:
+            with open(exp_data_path,'w') as f:
+                df_cw.to_csv(f,header=False)
+
+def carliniwagneruntargetedcochlear(audio_path,metadata_path,model_path,exp_data_path,adv_audio_path,save_data=False):
+    #Run the attacks to generate adversarial attacks on manually verified examples on the training and test data
+    #Load dataset to normalize new data
+    df = pd.read_csv(metadata_path)
+    label_names= df.iloc[:,2].values
+    file_names = df.iloc[:,1].values
+    mel_fb = librosa.filters.mel(sr=32000,n_fft=1024,n_mels=64).T
+    sample_rate = 32000
+    
+ 
+    audio_name = []
+    audio_length = []
+    original_label = []
+    original_confidence = []
+    new_label = []
+    new_confidence = []
+    new_o_label_conf = []
+    snr = []
+    with tf.Graph().as_default() as graph:
+        mel_filt = tf.convert_to_tensor(mel_fb,dtype=tf.float32)
+        model = CochlearModel(model_path +'.meta')
+        pcm = tf.placeholder(tf.float32,shape=[None,None],name='input_audio')
+        carliniwagner = CW.CarliniWagnerAttack(model,learning_rate = 5e-4,initial_const = 1e-2,max_iterations=500)
+        saver = carliniwagner.build_attack(pcm)
+    with tf.Session(graph=graph) as sess:
+        saver.restore(sess,model_path)
+        for i in range(df.shape[0]):
+            audio_file_name = file_names[i]
+            try:
+                data,q = utils_tf._preprocess_data(audio_path,audio_file_name)
+                data = np.expand_dims(data,axis=0)
+            except EOFError:
+                print("EOF Error")
+ 
+            label= utils_tf._convert_label_name_to_label(label_names[i])
+             
+            print('Ground truth label:',label_names[i],label)
+                
+                 
+         
+            
+            tic = time.process_time()
+            adv,o_label,o_conf,n_label,n_conf,n_conf_gt = carliniwagner.attack(sess,data,label,label,1)
+            toc = time.process_time()
+
+            print('Time for iteration:',i,'is',toc-tic)
+            adv = np.squeeze(adv)
+            if(save_data):
+                librosa.output.write_wav(adv_audio_path + 'adv-' + audio_file_name,adv,sample_rate)
+            
+            audio_name.append(audio_file_name)
+            audio_length.append(int(q))
+            original_label.append(o_label)
+            original_confidence.append(o_conf)
+            new_label.append(n_label)
+            new_confidence.append(n_conf)
+            new_o_label_conf.append(n_conf_gt)
+            snr.append(10*np.log10(np.mean(data**2)/(np.mean((adv-data)**2))))
+        if(save_data):
+            df_cw = pd.DataFrame({'audio_name':audio_name,'audio_length':audio_length,'original_label':original_label,'original_confidence':original_confidence,'new_label':new_label,'new_confidence':new_confidence,'new_orig_conf':new_o_label_conf,'SNR':snr})
+        
+            with open(exp_data_path,'w') as f:
                 df_cw.to_csv(f,header=False)
 
 

src/deepfool_main.py
@@ -14,7 +14,7 @@ import utils_tf
 import utils
 
 from attacks import deep_fool_method as DFM
-from cleverhans_wrapper import CleverHansModel
+from cleverhans_wrapper import CleverHansModel,CochlearModel
 
 
 def deepfoolattack(audio_path,metadata_path,model_path,exp_data_path,adv_audio_path,save_data=False):
@@ -46,7 +46,7 @@ def deepfoolattack(audio_path,metadata_path,model_path,exp_data_path,adv_audio_p
         deepfool.build_attack(pcm)
     with tf.Session(graph=graph) as sess:
         saver.restore(sess,model_path)
-        for i in range(df.shape[0]):
+        for i in range(2,3):
             audio_file_name = file_names[i]
             try:
                 data,q = utils_tf._preprocess_data(audio_path,audio_file_name)
@@ -87,11 +87,86 @@ def deepfoolattack(audio_path,metadata_path,model_path,exp_data_path,adv_audio_p
             new_label.append(np.argmax(preds))
             new_confidence.append(np.max(preds))
             new_o_label_conf.append(preds[np.argmax(s)])
-            snr.append(20*np.log10(np.mean(data**2)/(np.mean((adv-data)**2))))
+            snr.append(10*np.log10(np.mean(data**2)/(np.mean((adv-data)**2))))
         if(save_data):
             df_deepfool = pd.DataFrame({'audio_name':audio_name,'audio_length':audio_length,'original_label':original_label,'original_confidence':original_confidence,'new_label':new_label,'new_confidence':new_confidence,'new_orig_conf':new_o_label_conf,'SNR':snr})
         
             with open(exp_data_path,'a') as f:
                 df_deepfool.to_csv(f,header=False)
+
+
+def deepfoolcochlear(audio_path,metadata_path,model_path,exp_data_path,adv_audio_path,save_data=False):
+    #Run the attacks to generate adversarial attacks on manually verified examples on the training and test data
+    #Load dataset to normalize new data
+    df = pd.read_csv(metadata_path)
+    label_names= df.iloc[:,2].values
+    file_names = df.iloc[:,1].values
+    sample_rate = 32000
+    
+    audio_name = []
+    audio_length = []
+    original_label = []
+    original_confidence = []
+    new_label = []
+    new_confidence = []
+    new_o_label_conf = []
+    snr = []
+    with tf.Graph().as_default() as graph:
+        model = CochlearModel(model_path +'.meta')
+        pcm = tf.placeholder(tf.float32,shape=[None,None],name='input_audio')
+        saver= model.build_graph(pcm)
+        deepfool = DFM.DeepFool(model)
+        deepfool.build_attack(pcm)
+    with tf.Session(graph=graph) as sess:
+        saver.restore(sess,model_path)
+        for i in range(df.shape[0]):
+            audio_file_name = file_names[i]
+            try:
+                data,q = utils_tf._preprocess_data(audio_path,audio_file_name)
+                data = np.expand_dims(data,axis=0)
+            except EOFError:
+                print("EOF Error")
+ 
+            labels= utils_tf._convert_label_name_to_label(label_names[i])
+            s = sess.run([model.get_probs()],feed_dict={'input_audio:0':data})
+            
+            s = np.squeeze(s)
+            if (s.ndim != 1):
+                s = np.mean(s,axis=0)
+                      
+            print('Ground truth:',labels)    
+            print('Original label number:',np.argmax(s))
+            print('Original label confidence:',np.max(s))
+                
+            tic = time.process_time()
+            adv = deepfool.attack(sess,data,1)
+            toc = time.process_time()
+
+            print('Time for processing sample:',toc-tic,'for iteration:',i)
+            preds = sess.run([model.get_probs()],feed_dict={pcm:adv})
+            preds = np.squeeze(preds)
+
+            if(preds.ndim !=1):
+                preds = np.mean(preds,axis=0)
+            print('New label number:',np.argmax(preds))
+            print('New label confidence:',np.max(preds))
+            adv = np.squeeze(adv)    
+            if(save_data):
+                librosa.output.write_wav(adv_audio_path + 'adv-' + audio_file_name,adv,sample_rate)
+                
+            audio_name.append(audio_file_name)
+            audio_length.append(int(q))
+            original_label.append(np.argmax(s))
+            original_confidence.append(np.max(s))
+            new_label.append(np.argmax(preds))
+            new_confidence.append(np.max(preds))
+            new_o_label_conf.append(preds[np.argmax(s)])
+            snr.append(10*np.log10(np.mean(data**2)/(np.mean((adv-data)**2))))
+        if(save_data):
+            df_deepfool = pd.DataFrame({'audio_name':audio_name,'audio_length':audio_length,'original_label':original_label,'original_confidence':original_confidence,'new_label':new_label,'new_confidence':new_confidence,'new_orig_conf':new_o_label_conf,'SNR':snr})
+        
+            with open(exp_data_path,'a') as f:
+                df_deepfool.to_csv(f,header=False)
+
 
 